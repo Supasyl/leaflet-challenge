136a3cd27f831d888bce2c7ed85acc2a2c858cde
Supasyl <69235823+Supasyl@users.noreply.github.com>
2021-01-07 11:33:06 +0800

    step 1 map works

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,17 @@
+// creating map object
+var myMap = L.map('map').setView([37.8, -96], 4);
+
+// adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// load in geojson data
+var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+
+
